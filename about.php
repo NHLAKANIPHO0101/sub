821d9dac4d6073398a547d2d66b2fff3a3793c2c
NHLAKANIPHO0101 <135615087+NHLAKANIPHO0101@users.noreply.github.com>
2023-06-05 20:15:05 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UJ CAMPUS CONNECTORS</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<section id="header">
+        <a href="#"><img src="img/logo.png" class="logo" alt=""  style="width:80px;height:80px"></a>
+        <div >
+            <ul id="navbar">
+                <li><a href="index.php">Home</a></li>
+                <li><a  href="shop.php">Shop</a></li>
+                <li><a href="campus.php">Campus</a></li>
+                <li><a class="active" href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="cart.php"><i class="far fa-shopping-bag"></i></a></li>
+                <li><a href="user.php"><i class="fa fa-user-circle" aria-hidden="true"></i></a></li>
+
+            </ul>
+        </div>
+    </section>
+
+    <section id="about-header" >
+        <img src="about/pexels-athena-2962135.jpg" alt="" style="width:800px;height:400px">
+        <div id="about-detail" class="section-p1">
+            <h2>What Is UJ CAMPUS CONNECTORS</h2>
+            <div class="col">
+                <h4>A good community</h4>
+                <p>UJ campus Connector is a local online marketplace where students may sell, buy
+                    and collect unique items such as textbooks, clothes and electronics.We're also a 
+                    community working to make a difference for small business, students and young entrepreneurs.
+                </p>
+            </div>
+            <div class="col">
+                <h4>Support independent individuals</h4>
+                <p>There's no UJ Campus Connector warehouse, just students selling the things they love.
+                    We make the entire process easy by connecting you directly with merchants to uncover 
+                    something extraodinary.
+                </p>
+            </div class="col">
+            <div>
+                <h4>Peace of mind</h4>
+                <p>Our commited team places the highest priority on your privacy and if you ever need 
+                    assistance, we are always ready to step in to assist.
+                </p>
+            </div>
+        </div>
+
+    </section>
+
+    <footer class="section-p1">
+        <div class="col">
+            <h4>Contact</h4>
+            <p><strong>Address: </strong> 23Annet, Cottesloe, Johannesburg, 2092</p>
+            <p><strong>Phone: </strong> +27 (0) 11 559 4555  mylife@uj.ac.za</p>
+            <p><strong>Hours: </strong> 10:00 - 17:00, Mon - Sat</p>
+        </div>
+        <div class="col">
+            <h4>About</h4>
+            <a href="#">About us</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms & Conditions</a>
+            <a href="#">Contact Us</a>
+        </div>
+        <div class="col">
+            <h4>About</h4>
+            <a href="#">My Account</a>
+            <a href="#">Sign Up</a>
+            <a href="#">Campus</a>
+            <a href="#">My Wishlist</a>
+            <a href="#">Help</a>
+
+        </div>
+        <div class="col install">
+            <h4>Install</h4>
+            <p>From App Store or Google Play</p>
+            <div class="row">
+                <img src="img/Google.jpg" alt="">
+                <img src="img/apple.jpg" alt="">
+            </div>
+            <p>Secured Payment Gateways </p>
+            <img src="img/pay.png" alt="">
+
+        </div>
+        <div class="copyright">
+            <p>© 2023, Market Penetrators etc - UJ Marter Connectors</p>
+        </div>
+    </footer>
+
+
+    <script src="script.js"></script>
+</body>
+</html>
